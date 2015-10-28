9daff1fd0ad17c74abc52e768c6749aac9c5bf06
Dan Michaelis <dmichaelis0@gmail.com>
2015-10-28 19:41:51 -0400

    new files

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-import express from 'express'
-import HTTP from 'http'
-import Socket from 'socket.io'
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-const app = express();
-const http = HTTP.Server(app);
-const io = Socket(http);
-
-app.get('/', (req, res) => {
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
+io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
-http.listen(process.env.PORT || 3000, () => {
-  console.log('listening on *:3000');
+http.listen(5000, function(){
+  console.log('listening on *:5000');
 });
