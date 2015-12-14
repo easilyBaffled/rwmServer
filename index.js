a633aab3d4c78f540c8e52205ce057c071fbf115
Dan Michaelis <dmichaelis0@gmail.com>
2015-12-13 19:57:53 -0800

    clean up

--- index.js
+++ index.js
@@ -13,14 +13,11 @@ io.on('connection', function(socket){
   socket.on('newPos', function (pos) {
     console.log(pos);
     io.emit('newPos', pos);
-  })
-  socket.on('loaded', function (message) {
-    console.log(message);
-  })
+  });
   socket.on('test', function (message) {
     console.log(message);
     io.emit('test', message);
-  })
+  });
 });
 
 http.listen(process.env.PORT || 3000, function(){
