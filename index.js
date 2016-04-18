7366a9155fb72ed0275bf6c95b212a5aaab928db
Dan Michaelis <dmichaelis0@gmail.com>
2016-04-17 21:10:06 -0700

    scraping on the server

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Horseman = require('node-horseman');
+var horseman = new Horseman();
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  // socket.on('scrape', function(selectionData){
-  //   console.log('scraping');
-  //   horseman.open(selectionData.url).text(selectionData.selector)
-  //           .then(function (text) {
-  //             io.emit('selection', { path: selectionData.selector, text: text, url: selectionData.url});
-  //           })
-  //           .close();
-  // });
+  socket.on('scrape', function(selectionData){
+    console.log('scraping');
+    horseman.open(selectionData.url).text(selectionData.selector)
+            .then(function (text) {
+              io.emit('selection', { path: selectionData.selector, text: text, url: selectionData.url});
+            })
+            .close();
+  });
   socket.on('selection', function (selectionData) {
     console.log('recived data');
     io.emit('selection', selectionData);
